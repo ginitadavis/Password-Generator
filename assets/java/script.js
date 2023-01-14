a69f2b0b80567b81c29ec1c683de151fe8ce21fa
ginitadavis <120149064+ginitadavis@users.noreply.github.com>
2023-01-14 17:58:00 -0500

    Updated notes

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,7 +1,7 @@
 // Assignment code here
 
 //Declaration of variables
-var tagLength;
+var tagLength; //Length input from user
 var uppercaseLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowercaseLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var symbols = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "^", "_", "`", "{", "|", "}", "~"];
@@ -10,6 +10,7 @@ var tagUpperLetter;
 var tagLowerLetter;
 var tagSymbol;
 var tagNumber;
+var passwordElements = [];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -17,33 +18,23 @@ var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
 // Write password to the #password input
 function writePassword(password) {
+
+  //Calls function generatePassword and receives the result
   var password = generatePassword();
+
+  //Testing if function received the password
   console.log("This should show the generated password: "+ password);
   var passwordText = document.querySelector("#password");
-  console.log("This should show the generated password: "+ passwordText);
+
+  //Displays generated password into the textbox in the page
   passwordText.value = password;
 
 }
 
-/* COMMENT ALL THIS SECTION FOR NOW
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword());
-*/
-
-//************Practice section************
-//************Practice section************
-//************Practice section************
-
-
-
-//Adding all the possible elements of a password in one variable
-var passwordElements = [];
-
 function prompts() {
-  //Set up variables that will store user responses to wanting upper and lower letters, symbols, and numbers
+  //Variables will store user responses to wanting uppercase and lowercase letters, symbols, and numbers
   tagUpperLetter = confirm("Do you want upper case letters?");//Stores true or false
   tagLowerLetter = confirm("Do you want lower case letters?");//Stores true or false
   tagSymbol = confirm("Do you want symbols?");//Stores true or false
@@ -60,17 +51,22 @@ function prompts() {
 
 function generatePassword() {
 
+  //Declared and initialized a variable that will store the completed password
   var pass = "";
 
-  //Stores the password length entered by the user in variable tabLength if the variable is empty
+  //Stores the password length entered by the user in variable tagLength if the variable is empty
     tagLength = prompt("Please enter a password length between 8 and 128 characters");
 
   //If the lenght is below 8 or above 128 user must enter a new length
   if (tagLength < 8 || tagLength > 128) {
     window.alert("That's not a valid length. Please try again!")
-    generatePassword();
-  } else {
 
+    //If length is invalid, the function generatePassword is going to be called until the valid input is obtained
+    generatePassword();
+
+  } else { 
+    
+    //If the valid input is obtained then we will call the function prompts
     //Displays a series of prompts the user must answer
     prompts();
     
